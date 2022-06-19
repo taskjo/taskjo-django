d33bccc3b606a8d88ddd6a52e47540b9f4ec5f0b
Saee Raeisi <saeed.raesi2020@gmail.com>
2022-06-19 22:52:55 +0430

    [UPD] core.admin list view - filter

--- taskjo/core/admin.py
+++ taskjo/core/admin.py
@@ -1,13 +1,57 @@
 from django.contrib import admin
+from django.utils.html import format_html
 from .models import Projects, Category, Skill, Employer, Freelancer, Websites
+from taskjo.utils import jdatetime as jd
 # Register your models here.
-admin.site.register(Projects)
-admin.site.register(Category)
-admin.site.register(Skill)
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website',)
+    search_fields = ('name',)
+    list_select_related = ('website',)
+
+@admin.register(Skill)
+class SkillAdmin(admin.ModelAdmin):
+    list_display = ('name', 'website',)
+    search_fields = ('name',)
+    list_select_related = ('website',)
+
 admin.site.register(Employer)
 admin.site.register(Freelancer)
 
-#TODO add admin fro Projects
+@admin.register(Projects)
+class ProjectsAdmin(admin.ModelAdmin):
+    def remaining_time_j(self, obj):
+        return jd.pretty_jdatetime_format(obj.remaining_time)
+
+    def clickable_short_link(self, instance):
+        return format_html(
+            '<a href="{0}" target="_blank">{1}</a>',
+            instance.short_link,
+            'کلیک کنید'
+        )
+
+    def budget_format(self, instance):
+        budget = '{:,}'.format(int(instance.budget)/10)
+        return format_html(
+            '{0} تومان',
+            budget)
+
+    remaining_time_j.short_description = "مهلت انجام"
+    clickable_short_link.short_description = "لینک کوتاه"
+    budget_format.short_description = "بودجه به تومان"
+
+
+    list_display = (
+        'title', 'remaining_time_j', 'website', 'categoriy', 'clickable_short_link', 'budget_format',)
+    list_filter = (
+        'categoriy',
+        'skills',
+        'website',
+    )
+    list_select_related = ('website',)
+    search_fields = (
+        'title',)
+
 
 # admin.site.register(Websites)
 @admin.register(Websites)
